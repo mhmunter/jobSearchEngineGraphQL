8d813588a2c07ddb31d961af03a916696252bc92
mhmunter <mark.munter1@gmail.com>
2022-07-07 21:34:11 -0500

    updated jobs models

--- server/models/Job.js
+++ server/models/Job.js
@@ -1,4 +1,4 @@
-const { Schema } = require("mongoose");
+const { Schema, model} = require("mongoose");
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedJobs` array in User.js
 const jobSchema = new Schema({
@@ -24,29 +24,6 @@ const jobSchema = new Schema({
   },
 });
 
-// {
-//   type: String,
-// },
-// ],
-// description: {
-//   type: String,
-//   required: true,
-// },
-// // saved job id from GoogleJobs
-// jobId: {
-//   type: String,
-//   required: true,
-// },
-// image: {
-//   type: String,
-// },
-// link: {
-//   type: String,
-// },
-// title: {
-//   type: String,
-//   required: true,
-// },
 const Job = model("Job", jobSchema);
 
 module.exports = Job;
