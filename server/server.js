451230cfd32c3d819a945e1466f46436198dc50c
Sip <sipg789@gmail.com>
2022-07-12 20:28:25 -0500

    adding apollo server

--- server/server.js
+++ server/server.js
@@ -11,6 +11,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
